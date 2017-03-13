f59608b2c4ed9362e7cfef7e4de04f5958b22b4e
David Christie <davidchristie.nz@gmail.com>
2017-03-14 08:59:22 +1300

    Create welcome page

---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import Head from 'next/head'
+import React from 'react'
+import { Button, Grid, Jumbotron } from 'react-bootstrap'
+
+class Index extends React.Component {
+  render () {
+    return (
+      <div>
+        <Head>
+          <title>Housing Calculator</title>
+          <meta charSet='utf-8' />
+          <meta
+            content='initial-scale=1.0, width=device-width'
+            name='viewport'
+          />
+          <link
+            href='https://bootswatch.com/cosmo/bootstrap.min.css'
+            rel='stylesheet'
+          />
+        </Head>
+        <Grid>
+          <Jumbotron>
+            <h1>Housing Calculator</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <p>
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <Button bsStyle='primary'>Start</Button>
+          </Jumbotron>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default Index
